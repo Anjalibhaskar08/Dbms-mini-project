f48d14b3515cdf3835fba6b1a67b7f51116d32fb
Anjalibhaskar08 <anjalibhaskar08032003@gmail.com>
2024-02-24 22:31:32 +0530

    changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sqlite3
+from sqlalchemy import text
 from flask import Flask,render_template,request,session,redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import func
@@ -102,7 +103,9 @@ def index():
 @app.route('/home',methods=['POST','GET'])
 @login_required
 def home():
-    query=db.engine.execute(f"SELECT * FROM `category`;")
+    # query=db.engine.execute(f"SELECT * FROM `category`;")
+    with db.engine.connect() as conn:
+        query = conn.execute(text(f"SELECT * FROM `category`;")).fetchall()
     return render_template('item.html',query=query)
 
 @app.route('/cat',methods=['POST','GET'])
@@ -110,7 +113,9 @@ def home():
 def cat():
     if request.method=="POST":
         item=request.form.get('item')
-        query=db.engine.execute(f"INSERT INTO `category`(`item_name`) VALUES ('{item}');")
+        # query=db.engine.execute(f"INSERT INTO `category`(`item_name`) VALUES ('{item}');")
+        with db.engine.connect() as conn:
+            query = conn.execute(text(f"INSERT INTO `category`(`item_name`) VALUES ('{item}');")).fetchall()
         flash(item+" added","warning")
         return redirect(url_for('home'))
     return render_template('item.html')
@@ -124,12 +129,20 @@ def movement():
         frno=request.form.get('frno')
         trno=request.form.get('trno')
         if trno =="scrap":
-            query=db.engine.execute(f"DELETE FROM `item` WHERE `item_id`='{item}';")
+            # query=db.engine.execute(f"DELETE FROM `item` WHERE `item_id`='{item}';")
+            with db.engine.connect() as conn:
+                query = conn.execute(text(f"DELETE FROM `item` WHERE `item_id`='{item}';")).fetchall()
             flash(item+" deleted","warning")
             return redirect(url_for('home'))
-        query=db.engine.execute(f"UPDATE `item` SET `rno`= '{trno}' WHERE `item_id`='{item}';")
-        query=db.engine.execute(f"INSERT INTO `movement`(`item_id`, `from_rno`, `to_rno`) VALUES('{item}','{frno}','{trno}')")
-        cquery=db.engine.execute(f"SELECT * FROM `category`;")
+        # query=db.engine.execute(f"UPDATE `item` SET `rno`= '{trno}' WHERE `item_id`='{item}';")
+        with db.engine.connect() as conn:
+            query = conn.execute(text(f"UPDATE `item` SET `rno`= '{trno}' WHERE `item_id`='{item}';")).fetchall()
+        # query=db.engine.execute(f"INSERT INTO `movement`(`item_id`, `from_rno`, `to_rno`) VALUES('{item}','{frno}','{trno}')")
+        with db.engine.connect() as conn:
+            query = conn.execute(text(f"INSERT INTO `movement`(`item_id`, `from_rno`, `to_rno`) VALUES('{item}','{frno}','{trno}')")).fetchall()
+        # cquery=db.engine.execute(f"SELECT * FROM `category`;")
+        with db.engine.connect() as conn:
+            cquery = conn.execute(text(f"SELECT * FROM `category`;")).fetchall()
         flash(item+" moved from room "+frno+" to room "+trno,"warning")
         return render_template('item.html',query=cquery)
     
@@ -137,7 +150,9 @@ def movement():
 
 @app.route('/movement1',methods=['POST','GET'])
 def movement1():
-    query=db.engine.execute(f"SELECT * FROM `movement`;")
+    # query=db.engine.execute(f"SELECT * FROM `movement`;")
+    with db.engine.connect() as conn:
+        query = conn.execute(text(f"SELECT * FROM `movement`;")).fetchall()
     return render_template('movement1.html',query=query)
 
 
@@ -145,24 +160,44 @@ def movement1():
 @app.route('/dept',methods=['POST','GET'])
 @login_required
 def dept():
-    query0=db.engine.execute(f"SELECT * FROM `room`WHERE `rno`!='scrap' ;")
-    query1=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='ISE' ORDER BY i.item_id;;")
-    query2=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='ISE' ORDER BY i.item_id;;")
-    query3=db.engine.execute(f"SELECT * FROM `room`;")
+    # query0=db.engine.execute(f"SELECT * FROM `room`WHERE `rno`!='scrap' ;")
+    with db.engine.connect() as conn:
+        query0 = conn.execute(text(f"SELECT * FROM `room`WHERE `rno`!='scrap' ;")).fetchall()
+    # query1=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='ISE' ORDER BY i.item_id;;")
+    with db.engine.connect() as conn:
+        query1 = conn.execute(text(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='ISE' ORDER BY i.item_id;;")).fetchall()
+    # query2=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='ISE' ORDER BY i.item_id;;")
+    with db.engine.connect() as conn:
+        query2 = conn.execute(text(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='ISE' ORDER BY i.item_id;;")).fetchall()
+    # query3=db.engine.execute(f"SELECT * FROM `room`;")
+    with db.engine.connect() as conn:
+        query3 = conn.execute(text(f"SELECT * FROM `room`;")).fetchall()
     if request.method=="POST":
         name=request.form.get('dept')
         if name=='ISE':
-            query0=db.engine.execute(f"SELECT * FROM `room`WHERE `rno`!='scrap' ;")
-            query1=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")
-            query2=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")
-            query3=db.engine.execute(f"SELECT * FROM `room`;")
+            # query0=db.engine.execute(f"SELECT * FROM `room`WHERE `rno`!='scrap' ;")
+            with db.engine.connect() as conn:
+                query0 = conn.execute(text(f"SELECT * FROM `room`WHERE `rno`!='scrap' ;")).fetchall()
+            # query1=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")
+            with db.engine.connect() as conn:
+              query1 = conn.execute(text(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")).fetchall()
+            # query2=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")
+            with db.engine.connect() as conn:
+                query2 = conn.execute(text(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")).fetchall()
+            # query3=db.engine.execute(f"SELECT * FROM `room`;")
+            with db.engine.connect() as conn:
+              query2 = conn.execute(text(f"SELECT * FROM `room`;")).fetchall()
             return render_template('dept.html',query=[query0,query1,query2,query3])
 
         elif name.lower()=='cse':
-            query=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")
+            # query=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")
+            with db.engine.connect() as conn:
+                query = conn.execute(text(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")).fetchall()
             return render_template('dept.html',query=query)
         elif name.lower()=='ece':
-            query=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")
+            # query=db.engine.execute(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")
+            with db.engine.connect() as conn:
+                query = conn.execute(text(f"SELECT i.item_id,i.rno FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND d.dname='{name}' ORDER BY i.item_id;;")).fetchall()
             return render_template('dept.html',query=query)
         else:
             flash("Invalid dept","deanger")
@@ -174,12 +209,20 @@ def dept():
 @app.route('/out',methods=['POST','GET'])
 @login_required
 def out():
-    query0=db.engine.execute(f"SELECT * FROM `room` WHERE `rno`!='scrap';")
-    query3=db.engine.execute(f"SELECT `dno` FROM `room` WHERE `rno`!='scrap';")
+    # query0=db.engine.execute(f"SELECT * FROM `room` WHERE `rno`!='scrap';")
+    with db.engine.connect() as conn:
+        query0 = conn.execute(text(f"SELECT * FROM `room` WHERE `rno`!='scrap';")).fetchall()
+    # query3=db.engine.execute(f"SELECT `dno` FROM `room` WHERE `rno`!='scrap';")
+    with db.engine.connect() as conn:
+        query3 = conn.execute(text(f"SELECT `dno` FROM `room` WHERE `rno`!='scrap';")).fetchall()
     if request.method=="POST":
         rno=request.form.get('rno')
-        query1=db.engine.execute(f"SELECT * FROM `room` WHERE `rno`!='scrap';")
-        query2=db.engine.execute(f"SELECT i.item_id,i.item_name,i.rno,d.dname FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND i.rno='{rno}' ORDER BY i.item_id;")
+        # query1=db.engine.execute(f"SELECT * FROM `room` WHERE `rno`!='scrap';")
+        with db.engine.connect() as conn:
+            query1 = conn.execute(text(f"SELECT * FROM `room` WHERE `rno`!='scrap';")).fetchall()
+        # query2=db.engine.execute(f"SELECT i.item_id,i.item_name,i.rno,d.dname FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND i.rno='{rno}' ORDER BY i.item_id;")
+        with db.engine.connect() as conn:
+              query2 = conn.execute(text(f"SELECT i.item_id,i.item_name,i.rno,d.dname FROM item i, dept d,room r where d.dno=r.dno AND i.rno=r.rno AND i.rno='{rno}' ORDER BY i.item_id;")).fetchall()
         return render_template('out.html',query=[query1,query2])
     return render_template('out.html',query=[query0,query3])
 
@@ -190,9 +233,15 @@ def out():
 def item():
     if request.method=="POST":
         name=request.form.get('dept')
-        cquery=db.engine.execute(f"SELECT * FROM `category`;")
-        rquery=db.engine.execute(f"SELECT * FROM `room` WHERE `rno`!='scrap';")
-        vquery=db.engine.execute(f"SELECT * FROM `vender`;")
+        # cquery=db.engine.execute(f"SELECT * FROM `category`;")
+        with db.engine.connect() as conn:
+            cquery = conn.execute(text(f"SELECT * FROM `category`;")).fetchall()
+        # rquery=db.engine.execute(f"SELECT * FROM `room` WHERE `rno`!='scrap';")
+        with db.engine.connect() as conn:
+            rquery = conn.execute(text(f"SELECT * FROM `room` WHERE `rno`!='scrap';")).fetchall()
+        # vquery=db.engine.execute(f"SELECT * FROM `vender`;")
+        with db.engine.connect() as conn:
+            vquery = conn.execute(text(f"SELECT * FROM `vender`;")).fetchall()
         if name.lower()=='ise':
             return render_template('ise.html',query=[cquery,rquery,vquery])
         elif name.lower()=='cse':
@@ -229,8 +278,12 @@ def ise():
 
             itemid=item+x
 
-            quer=db.engine.execute(f"INSERT INTO `item`(`item_id`, `item_name`, `rno`, `vid`) VALUES('{itemid}','{itemm}','{rno}','{vid}');")
-            query=db.engine.execute(f"SELECT * FROM `category`;")
+            # quer=db.engine.execute(f"INSERT INTO `item`(`item_id`, `item_name`, `rno`, `vid`) VALUES('{itemid}','{itemm}','{rno}','{vid}');")
+            with db.engine.connect() as conn:
+                query = conn.execute(text(f"INSERT INTO `item`(`item_id`, `item_name`, `rno`, `vid`) VALUES('{itemid}','{itemm}','{rno}','{vid}');"))
+            # query=db.engine.execute(f"SELECT * FROM `category`;")
+            with db.engine.connect() as conn:
+                query = conn.execute(text(f"SELECT * FROM `category`;")).fetchall()
             flash(itemm+" added succesfully","warning")
             return render_template('item.html',query=query)
 
@@ -252,7 +305,9 @@ def signin():
             return render_template('signin.html')
         encpass=generate_password_hash(password)
 
-        newuser=db.engine.execute(f"INSERT INTO `user` (`name`,`email`,`password`) VALUES ('{name}','{email}','{encpass}')")
+        # newuser=db.engine.execute(f"INSERT INTO `user` (`name`,`email`,`password`) VALUES ('{name}','{email}','{encpass}')")
+        with db.engine.connect() as conn:
+            newuser = conn.execute(text(f"INSERT INTO `user` (`name`,`email`,`password`) VALUES ('{name}','{email}','{encpass}')")).fetchall()
         flash("Sign in Success Please Login","sucess")
         return render_template('login.html')
 
@@ -270,11 +325,16 @@ def login():
         if user and check_password_hash(user.password,password):
             login_user(user)
             flash("Login Success","primary")
-            from sqlalchemy import text
-            with db.engine.begin() as conn:
-             result = conn.execute(text(sqlite3)) 
-             conn.commit() 
-            query=query=db.engine.execute(f"SELECT * FROM `category`;")
+            # from sqlalchemy import text
+            # with db.engine.begin() as conn:
+            #  result = conn.execute(text(sqlite3)) 
+            #  conn.commit() 
+            print("engine",db.engine)
+            # query=db.engine.execute(f"SELECT * FROM `category`;")
+            with db.engine.connect() as conn:
+                query = conn.execute(text(f"SELECT * FROM `category`;")).fetchall()
+                # query=conn.execute(f"SELECT * FROM `category`;")
+            print("query",query)
             return render_template('item.html',query=query)
         else:
             flash("Invalid User Id or password","danger")
@@ -285,19 +345,27 @@ def login():
 @app.route('/log')
 @login_required
 def log():
-    query=db.engine.execute(f"SELECT * FROM `logs`;")
+    # query=db.engine.execute(f"SELECT * FROM `logs`;")
+    with db.engine.connect() as conn:
+        query = conn.execute(text(f"SELECT * FROM `logs`;")).fetchall()
     return render_template('logs.html',query=query)
 
 @app.route('/van',methods=['POST','GET'])
 @login_required
 def van():
-    query=db.engine.execute(f"SELECT * FROM `vender`;")
+    # query=db.engine.execute(f"SELECT * FROM `vender`;")
+    with db.engine.connect() as conn:
+        query = conn.execute(text(f"SELECT * FROM `vender`;")).fetchall()
     if request.method=="POST":
         vid=request.form.get('vid')
         vname=request.form.get('vname')
-        query=db.engine.execute(f"INSERT INTO `vender`(`vid`, `name`, `billing`) VALUES ('{vid}','{vname}','NULL');")
+        # query=db.engine.execute(f"INSERT INTO `vender`(`vid`, `name`, `billing`) VALUES ('{vid}','{vname}','NULL');")
+        with db.engine.connect() as conn:
+            query = conn.execute(text(f"INSERT INTO `vender`(`vid`, `name`, `billing`) VALUES ('{vid}','{vname}','NULL');")).fetchall()
         flash(" new vendor added","warning")
-        query1=db.engine.execute(f"SELECT * FROM `vender`;")
+        # query1=db.engine.execute(f"SELECT * FROM `vender`;")
+        with db.engine.connect() as conn:
+            query1 = conn.execute(text(f"SELECT * FROM `vender`;")).fetchall()
         return render_template('vender.html',query=query1)
     return render_template('vender.html',query=query)
 
